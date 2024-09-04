a405ba26a32b07fe0058d46cc4562d4a2cde5eb2
Sophia White <sophiaewhite303@gmail.com>
2024-09-04 23:08:37 +0000

    Working code!!

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,25 +1,34 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class InsSortInt {
-    public static void main (String[] args){
-        int[] numsArray = new int [1000];
-        Scanner scan = new Scanner(new File("randInts.txt"));
-        for (int i = 0; i<numsArray.length; i++){
-            numsArray[i] = scan.nextInt();
+    public static void main(String[] args) {
+        int[] numsArray = new int[1000];
+        try {
+            Scanner scan = new Scanner(new File("randInts.txt"));
+            int i = 0;
+            while (scan.hasNextInt() && i < numsArray.length) {
+                numsArray[i] = scan.nextInt();
+                i++;
+            }
+            scan.close();
+            sort(numsArray, i); // Pass the number of valid elements to the sort method
+            System.out.println(numsArray[0] + ":" + numsArray[Math.min(999, i - 1)]);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        sort(numsArray);
-        System.out.println(numsArray[0] + ":" + numsArray[999]);
     }
 
-    public static void sort (int[] array){
-        for (int i =1; i<array.length; i++){
+    public static void sort(int[] array, int size) {
+        for (int i = 1; i < size; i++) {
             int current = array[i];
-            int first = i-1;
-            while (array[first] >=0 && array[first] >= current){
-                first = current;
-                current = (first-1);
-                first--;
+            int j = i - 1;
+            // Move elements of array[0..i-1] that are greater than current to one position ahead
+            while (j >= 0 && array[j] > current) {
+                array[j + 1] = array[j];
+                j--;
             }
+            array[j + 1] = current;
         }
     }
 }
